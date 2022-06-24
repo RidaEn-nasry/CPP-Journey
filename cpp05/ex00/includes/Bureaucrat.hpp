140a9dbd005be11d1532c4eeb5559d7471b8c09c
Rida En-nasry <ren-nasr@e2r4p14.1337.ma>
2022-06-24 11:08:33 +0100

    cpp05 init

---
+++ cpp05/ex00/includes/Bureaucrat.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ren-nasr <ren-nasr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/24 08:33:57 by ren-nasr          #+#    #+#             */
+/*   Updated: 2022/06/24 08:39:01 by ren-nasr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+
+class   Bureaucrat {
+    private:
+        std::string name;
+        int grade;
+    
+}
+
+
+#endif
