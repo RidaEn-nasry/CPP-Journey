b44b5bc030211165ea3e0bdfa593a31f21019d62
wa5ina <ren-nasr@student.1337.ma>
2022-06-27 20:23:28 +0100

    cpp06

--- cpp06/ex00/practice.cpp
+++ cpp06/ex00/practice.cpp
@@ -3,24 +3,20 @@
 #include <limits.h>
 #include <iostream>
 
-int main(int argc, char **argv)
+int main( void )
 {
     // overflowing a float number
         
 
     // biggest number that can be represented by a 32-bit float is 3.40282347e+38f (FLT_MAX) 
 
-    // changing a float number to binary representation
-    float f  = 3.40282347e+38f;
-    int bits[32];
-    for (int i = 0; i < 32; i++)
-    {
-        bits[i] = f 
-    }
-    for (int i = 0; i < 32; i++)
-    {
-        std::cout << bits[i];
-    }
+//    example of lose of precision when converting a float to a double:
+    double d = 3.14159265358979323846;
+   float f = 3.14159265358979323846f;
+//    double d = f;
+   std::cout << "f: " << f << std::endl;
+   std::cout << "d: " << d << std::endl;
+//    std::cout << "f - d: " << f - d << std::endl;
 
 
 }
