a9440e8cc1ebda400f1de033d4b09ad8797dcad7
wa5ina <ren-nasr@student.1337.ma>
2022-07-03 21:46:40 +0100

    cpp07 finished

--- cpp07/ex02/includes/Array.hpp
+++ cpp07/ex02/includes/Array.hpp
@@ -1,26 +1,70 @@
-Synthesizing 0/10 solutions (Duplicates hidden)
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ren-nasr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/02 13:45:31 by ren-nasr          #+#    #+#             */
-/*   Updated: 2022/07/02 13:45:38 by ren-nasr         ###   ########.fr       */
+/*   Created: 2022/07/03 08:37:22 by ren-nasr          #+#    #+#             */
+/*   Updated: 2022/07/03 21:37:18 by ren-nasr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
+#include <cstdlib>
+#include <iostream>
+template < typename T >
+class Array {
+       private:
+	T* _array;
+	unsigned int _size;
 
-template <typename T>
+       public:
+	Array( void ) {
+		this->_array = NULL;
+		this->_size = 0;
+	}
+	Array( unsigned int n ) {
+		try {
+			this->_array = new T [ n ]( );
+		} catch ( std::exception& e ) {
+			std::cout << "new failed:\t" << e.what( ) << std::endl;
+			exit( EXIT_FAILURE );
+		}
+		this->_size = n;
+	}
+	~Array( void ) { delete [] this->_array; }
+	Array( const Array< T >& other ) { *this = other; }
+	Array& operator=( const Array< T >& rhs ) {
+		if ( rhs.size( ) == 0 ) {
+			this->_array = NULL;
+			this->_size = 0;
+			return *this;
+		}
+		try {
+			this->_array = new T [ rhs.size( ) ];
+			for ( size_t i = 0; i < rhs.size( ); i++ ) this->_array [ i ] = rhs._array [ i ];
+			this->_size = rhs._size;
+			return *this;
+		} catch ( std::exception& e ) {
+			std::cout << e.what( ) << std::endl;
+			exit( EXIT_FAILURE );
+		}
+	}
+	size_t size( void ) const { return this->_size; };
 
-class	Array {
-	public:
-		Array ( void );
-		Array ( unsigned int n );
-		Array ( Array const & src )
-
-}
+	T& operator[]( unsigned int index ) {
+		if ( index > ( this->_size - 1 ) )
+			throw OutOfRangeException( );
+		else {
+			return this->_array [ index ];
+		}
+	}
+	class OutOfRangeException : public std::exception {
+		//using OutOfRangeException::what;
+		virtual const char* what( ) const throw( ) { return "array subscript is out of range"; }
+	};
+};
 
 #endif
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -1,58 +1,110 @@
-#include <iostream>
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ren-nasr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/03 10:49:04 by ren-nasr          #+#    #+#             */
+/*   Updated: 2022/07/03 21:39:01 by ren-nasr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <Array.hpp>
+#include <iostream>
+#define MAX_VAL 750
+
 int main( void ) {
+	std::cout << "**********************************************************" << std::endl;
+	std::cout << "*			Instantiation Tests			*" << std::endl;
+	std::cout << "**********************************************************" << std::endl;
+	// ints
+	Array< int > intArr;
+	if ( intArr.size( ) == 0 ) std::cout << "array of integers is empty " << std::endl;
 
-	Array a;
+	// doubles
+	Array< double > doubleArr;
+	if ( doubleArr.size( ) == 0 ) std::cout << "array of floats is empty " << std::endl;
 
+	// floats
+	Array< float > floatArr;
+	if ( floatArr.size( ) == 0 ) std::cout << "array of doubles is empty" << std::endl;
+
+	// chars
+	Array< char > charArr( 3 );
+	std::cout << charArr.size( ) << std::endl;
+
+	// strings
+	Array< std::string > stringArr( 199 );
+	std::cout << charArr.size( ) << std::endl;
+	std::cout << "**********************************************************" << std::endl;
+	std::cout << "*			Functionalities Tests		       *" << std::endl;
+	std::cout << "**********************************************************" << std::endl;
+
+	// copy constructor
+	srand( time( NULL ) );
+	Array< float > floats( 5 );
+	for ( size_t i = 0; i < floats.size( ); i++ ) floats [ i ] = rand( ) % 50;
+	Array< float > copyFloats( floats );
+	std::cout << "Values of floats array: ";
+	for ( size_t i = 0; i < floats.size( ); i++ ) std::cout << floats [ i ] << " ";
+	std::cout << "is the same as values of it's copy: ";
+	for ( size_t i = 0; i < copyFloats.size( ); i++ ) std::cout << copyFloats [ i ] << " ";
+	std::cout << std::endl;
+	// assignment operator
+	floatArr = copyFloats;
+	std::cout << "floatArr before assignement is == NULL" << std::endl;
+	std::cout << "floatArr after assignment is: ";
+	for ( size_t i = 0; i < copyFloats.size( ); i++ ) std::cout << floatArr [ i ] << " ";
+	std::cout << std::endl;
+
+	// out of range tests
+	try {
+		std::cout << floatArr [ 100 ];
+	} catch ( std::exception& e ) {
+		std::cout << e.what( ) << std::endl;
+	}
+
+	std::cout << "**********************************************************" << std::endl;
+	std::cout << "*			Mandatory Tests			"
+		     "	*"
+		  << std::endl;
+	std::cout << "**********************************************************" << std::endl;
+
+	Array< int > numbers( MAX_VAL );
+	int* mirror = new int [ MAX_VAL ];
+	srand( time( NULL ) );
+	for ( int i = 0; i < MAX_VAL; i++ ) {
+		const int value = rand( );
+		numbers [ i ] = value;
+		mirror [ i ] = value;
+	}
+	// SCOPE
+	{
+		Array< int > tmp = numbers;
+		Array< int > test( tmp );
+	}
+
+	for ( int i = 0; i < MAX_VAL; i++ ) {
+		if ( mirror [ i ] != numbers [ i ] ) {
+			std::cerr << "didn't save the same value!!" << std::endl;
+			return 1;
+		}
+	}
+	try {
+		numbers [ -2 ] = 0;
+	} catch ( const std::exception& e ) {
+		std::cerr << e.what( ) << '\n';
+	}
+	try {
+		numbers [ MAX_VAL ] = 0;
+	} catch ( const std::exception& e ) {
+		std::cerr << e.what( ) << '\n';
+	}
+
+	for ( int i = 0; i < MAX_VAL; i++ ) {
+		numbers [ i ] = rand( );
+	}
+	delete [] mirror;
 	return 0;
 }
-//#define MAX_VAL 750
-//int main(int, char**)
-{
-//    Array<int> numbers(MAX_VAL);
- //   int* mirror = new int[MAX_VAL];
-  //  srand(time(NULL));
-   // for (int i = 0; i < MAX_VAL; i++)
-    //{
-     //   const int value = rand();
-      //  numbers[i] = value;
-       // mirror[i] = value;
-    //}
-    ////SCOPE
-    //{
-     //   Array<int> tmp = numbers;
- //       Array<int> test(tmp);
-    //}
-//
- //   for (int i = 0; i < MAX_VAL; i++)
-  //  {
-   //     if (mirror[i] != numbers[i])
-    //    {
-     //       std::cerr << "didn't save the same value!!" << std::endl;
-      //      return 1;
-       // }
-    //}
-    //try
-    //{
-     //   numbers[-2] = 0;
-    //}
-   // catch (const std::exception& e)
-   // {
-    //    std::cerr << e.what() << '\n';
-    }
-  //  try
-   // {
-     //   numbers[MAX_VAL] = 0;
-    //}
-    //catch (const std::exception& e)
-   // {
-    //    std::cerr << e.what() << '\n';
-  //  }
-
- //   for (int i = 0; i < MAX_VAL; i++)
- //   {
-//        numbers[i] = rand();
-//    }
-//    delete[] mirror;//
- //   return 0;
-//}
